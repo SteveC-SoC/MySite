32ce7a6c19330db6a72bccd8be7c44fca6046b99
Steve Cadden <s.p.cadden@gmail.com>
2021-07-06 16:49:30 +0100

    Icons are in and showing correctly

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,5 +1,7 @@
 import { useState} from 'react';
 import "./homePage.css";
+import {FaTwitter, FaGithub, FaLinkedin, FaPlus} from "react-icons/fa";
+import {IconContext} from "react-icons";
 
 
 
@@ -21,19 +23,28 @@ function HomePage() {
           projects
         </a>
         <a href="https://twitter.com/of_gloin" rel="noreferrer" target="_blank" className="navLink">
-          Twitter
+            <IconContext.Provider value={{style: {fontSize: '30px', color: "rgb(0, 0, 0)"}}}>
+                <FaTwitter />
+            </IconContext.Provider>
+         
         </a>
         <a href="https://github.com/SteveC-SoC" rel="noreferrer" target="_blank" className="navLink">
-            github
+         <IconContext.Provider value={{style: {fontSize: '30px', color: "rgb(0, 0, 0)"}}}>
+            <FaGithub />
+         </IconContext.Provider>
         </a>
         <a href="https://www.linkedin.com/in/steve-cadden/" rel="noreferrer" target="_blank" className="navLink">
-          LinkedIn
+         <IconContext.Provider value={{style: {fontSize: '30px', color: "rgb(0, 0, 0)"}}}>
+            <FaLinkedin/>
+         </IconContext.Provider>
         </a>
         <a href="#" className="navLink">
           blog
         </a>
       </div>
-        <div className="navButton" onClick={handleClick}>+</div>
+        <div className="navButton" onClick={handleClick}>
+            <FaPlus/>
+        </div>
     </div>
   );
 }
